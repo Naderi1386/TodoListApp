42e3cd860439af8cfc0be56e2a467eabee616315
unknown <mh.naderi1386@gmail.com>
2025-07-08 15:42:27 +0330

    added icons

--- components/ui/BottomTab.tsx
+++ components/ui/BottomTab.tsx
@@ -4,34 +4,42 @@ import { useState } from "react";
 import { StyleSheet } from "react-native";
 import { BottomNavigation } from "react-native-paper";
 import type { NativeStackNavigationProp } from "@react-navigation/native-stack";
-
 import { StackParamList } from "@/app/_layout";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 type NavigationProp = NativeStackNavigationProp<StackParamList>;
 type RouteKey = "AddTasks" | "Home" | "Tasks";
 
+interface Route {
+  key: RouteKey;
+  title: string;
+  icon: string;
+}
+
 const BottomTab = () => {
   const navigation = useNavigation<NavigationProp>();
   const [index, setIndex] = useState(0);
-  const [routes] = useState<{ key: RouteKey; title: string }[]>([
-    { key: "Home", title: "Home" },
-    { key: "Tasks", title: "Tasks" },
-    {
-      key: "AddTasks",
-      title: "Add Tasks",
-    },
+
+  const [routes] = useState<Route[]>([
+    { key: "Home", title: "Home", icon: "home" },
+    { key: "Tasks", title: "Tasks", icon: "clipboard-list" },
+    { key: "AddTasks", title: "Add Tasks", icon: "plus" },
   ]);
 
   const onIndexChange = (index: number) => {
     setIndex(index);
     const routeKey = routes.at(index)?.key;
-    navigation.navigate(routeKey as RouteKey);
+    if (routeKey) {
+      navigation.navigate(routeKey);
+    }
   };
 
   const renderScene = () => null;
 
   return (
     <BottomNavigation
+      shifting={false}
+      sceneAnimationEnabled={false}
       barStyle={Styles.bottomTab}
       style={{ flex: 0.1 }}
       inactiveColor={Colors.grey}
@@ -39,6 +47,28 @@ const BottomTab = () => {
       navigationState={{ index, routes }}
       onIndexChange={onIndexChange}
       renderScene={renderScene}
+      renderIcon={({ route, color }) => {
+        switch (route.key) {
+          case "Home":
+            return (
+              <MaterialCommunityIcons name="home" color={color} size={30} />
+            );
+          case "Tasks":
+            return (
+              <MaterialCommunityIcons
+                name="clipboard-list"
+                color={color}
+                size={30}
+              />
+            );
+          case "AddTasks":
+            return (
+              <MaterialCommunityIcons name="plus" color={color} size={30} />
+            );
+          default:
+            return null;
+        }
+      }}
     />
   );
 };
@@ -47,7 +77,6 @@ export default BottomTab;
 
 const Styles = StyleSheet.create({
   bottomTab: {
-    color: "white",
     backgroundColor: Colors.blueDark,
   },
 });
